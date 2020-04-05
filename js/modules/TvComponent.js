8bc11000979094df51e93f14381d45f67967856e
Philip <philipwood0912@gmail.com>
2020-04-05 04:10:14 -0400

    update pages

--- js/modules/TvComponent.js
+++ js/modules/TvComponent.js
@@ -1,14 +1,113 @@
 import ProfileComponent from './ProfileComponent.js';
+import PopupComponent from './PopupComponent.js';
+import LeftButton from './LeftButton.js';
+import RightButton from './RightButton.js';
 export default {
     template: `
-        <div v-if="this.$parent.profilepick">
-            <h2>TV Page</h2>
+    <div v-if="this.$parent.profilepick">
+        <div id="home-content">
+            <h2 class="title">TV Shows - 2000s</h2>
+            <div class="home-content">
+                <div class="home-wrp">
+                    <popup v-for="(movie, index) in this.showsTen" :obj="movie" :key="index" :offset="index">
+                    </popup>
+                </div>
+                <div class="home-buttons">
+                    <left :num="1" :array="this.showsTen"></left>
+                    <right :num="0" :array="this.showsTen"></right>
+                </div>
+            </div>
+            <h2 class="title">TV Shows - 1990s</h2>
+            <div class="home-content">
+                <div class="home-wrp">
+                    <popup v-for="(movie, index) in this.showsNine" :obj="movie" :key="index" :offset="index">
+                    </popup>
+                </div>
+                <div class="home-buttons">
+                    <left :num="1" :array="this.showsNine"></left>
+                    <right :num="0" :array="this.showsNine"></right>
+                </div>
+            </div>
+            <h2 class="title">TV Shows - 1980s</h2>
+            <div class="home-content">
+                <div class="home-wrp">
+                    <popup v-for="(movie, index) in this.showsEight" :obj="movie" :key="index" :offset="index">
+                    </popup>
+                </div>
+                <div class="home-buttons">
+                    <left :num="1" :array="this.showsEight"></left>
+                    <right :num="0" :array="this.showsEight"></right>
+                </div>
+            </div>
+            <h2 class="title">TV Shows - 1970s</h2>
+            <div class="home-content">
+                <div class="home-wrp">
+                    <popup v-for="(movie, index) in this.showsSeven" :obj="movie" :key="index" :offset="index">
+                    </popup>
+                </div>
+                <div class="home-buttons">
+                    <left :num="1" :array="this.showsSeven"></left>
+                    <right :num="0" :array="this.showsSeven"></right>
+                </div>
+            </div>
+            <h2 class="title">TV Shows - 1960s</h2>
+            <div class="home-content">
+                <div class="home-wrp">
+                    <popup v-for="(movie, index) in this.showsSix" :obj="movie" :key="index" :offset="index">
+                    </popup>
+                </div>
+                <div class="home-buttons">
+                    <left :num="1" :array="this.showsSix"></left>
+                    <right :num="0" :array="this.showsSix"></right>
+                </div>
+            </div>
         </div>
-        <div v-else>
-            <profiles></profiles>
-        </div>
+    </div>
+    <div v-else>
+    <profiles></profiles>
+    </div>
     `,
     components: {
-        profiles: ProfileComponent
+        profiles: ProfileComponent,
+        popup: PopupComponent,
+        right: RightButton,
+        left: LeftButton
+    },
+    data: function(){
+        return {
+            showsTen: [],
+            showsNine: [],
+            showsEight: [],
+            showsSeven: [],
+            showsSix: [],
+            showinfo: ""
+        }
+    },
+    methods: {
+        pullShows(int1, int2, arr){
+            let url = `https://api.themoviedb.org/3/discover/tv?api_key=6c056957a2e9be6e0e41a303073bae05&language=en-US&sort_by=popularity.desc&first_air_date.gte=${int1}-01-01&first_air_date.lte=${int2}-12-31&page=1&timezone=America%2FNew_York&vote_average.gte=7&without_genres=16%2C%2010751&include_null_first_air_dates=false`;
+            fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                for(var i = 0; i < data.results.length; i++){
+                    if(data.results[i].poster_path == null){
+                        continue;
+                    } else{
+                        arr.push(data.results[i]);
+                        delete Object.assign(arr[i], {['release_date']: arr[i]['first_air_date'] })['first_air_date'];
+                        delete Object.assign(arr[i], {['title']: arr[i]['name'] })['name'];
+                    }
+                }
+            })
+            .catch(err => console.log(err))
+        },
+
+    },
+    created: function(){
+        this.pullShows(2000, 2019, this.showsTen);
+        this.pullShows(1990, 1999, this.showsNine);
+        this.pullShows(1980, 1989, this.showsEight);
+        this.pullShows(1970, 1979, this.showsSeven);
+        this.pullShows(1960, 1969, this.showsSix);
     }
 }
